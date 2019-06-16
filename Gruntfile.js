527abe0a9635bed5a6dcf8ef275589046c025cc2
queen <mecksee@gmail.com>
2019-06-15 19:17:47 -0600

    indentation error

--- Gruntfile.js
+++ Gruntfile.js
@@ -93,7 +93,7 @@ module.exports = function( grunt ) {
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	// Register tasks
 	grunt.registerTask( 'default', [
