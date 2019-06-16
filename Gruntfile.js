5636c677fbb70463ee349909af8737525b08394a
queen <mecksee@gmail.com>
2019-06-15 19:16:18 -0600

    Setting up Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,121 @@
+module.exports = function( grunt ) {
+	'use strict';
+
+	// Load all tasks
+	require( 'load-grunt-tasks' )( grunt, { scope: 'devDependencies' } );
+
+	grunt.initConfig( {
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all:     [
+				'Gruntfile.js',
+				'js/**/*.js',
+				'!js/dist/*.js',
+				'!js/*.min.js'
+			]
+		},
+
+		babel: {
+			options: {
+				'sourceMap': true,
+				presets: ['@babel/preset-env']
+			},
+			dist: {
+				files: [{
+					'expand': true,
+					'cwd': 'src/js',
+					'src': ['**/*.js'],
+					'dest': 'dist/js/',
+					'ext': '.min.js'
+				}]
+			}
+		},
+
+		uglify: {
+			all_src : {
+				options : {
+				  sourceMap : true
+				},
+				src : 'dist/**/*.min.js',
+				dest : 'dist/js/bundle.min.js'
+			}
+		},
+
+		sass: {
+			dist: {
+				options: {
+					sourceMap: true,
+					outputStyle:  'expanded'
+				},
+				files:   [ {
+					expand: true,
+					cwd:    'src/scss',
+					src: [
+						'*.scss'
+					],
+					dest:   'dist/css',
+					ext: '.css'
+				} ]
+			},
+			minify: {
+				options: {
+					sourceMap: true,
+					outputStyle:  'compressed'
+				},
+				files:   [ {
+					expand: true,
+					cwd:    'dist/css',
+					src: [
+						'*.css'
+					],
+					dest:   'dist/css',
+					ext: '.min.css'
+				} ]
+			}
+		},
+
+		watch: {
+			sass: {
+				options: {
+					livereload: true
+				},
+				files: [ 'src/scss/*.scss' ],
+				tasks: [ 'build:css' ]
+			}
+		}
+
+	} );
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-babel');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'build:js',
+		'build:css'
+	] );
+
+	// Build tasks
+	grunt.registerTask( 'build', [
+		'default'
+	] );
+
+	// Build JS
+	grunt.registerTask( 'build:js', [
+		'jshint',
+		'babel',
+		'uglify'
+	] );
+
+	// Build CSS
+	grunt.registerTask( 'build:css', [
+		'sass'
+	] );
+
+};
---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+const cake = [
+    {
+        id: '1',
+        type: 'chocolate'
+    },
+    {
+        id: '2',
+        type: 'strawberry'
+    }
+];
+
+for( let cakes of cake ){
+    console.log(cakes.type);
+}
+
+console.log(cake);
