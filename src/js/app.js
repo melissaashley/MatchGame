98374bd5f6c47673a752f500a9606840168d004b
queen <mecksee@gmail.com>
2019-06-24 18:55:48 -0600

    generate cards and randomize

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,62 @@
-let grabCards = document.getElementsByClassName( 'tile' );
-let cards = Array.from( grabCards );
+const cards = [
+  {
+    id: '1',
+    name: 'frog'
+  },
+  {
+    id: '2',
+    name: 'bird'
+  },
+  {
+    id: '3',
+    name: 'fish'
+  },
+  {
+    id: '4',
+    name: 'goose'
+  },
+  {
+    id: '5',
+    name: 'moose'
+  },
+  {
+    id: '6',
+    name: 'parrot'
+  },
+  {
+    id: '7',
+    name: 'lion'
+  },
+  {
+    id: '8',
+    name: 'tiger'
+  }
+]
+
+const game = document.getElementById('game');
+let gridBoard = cards.concat(cards);
+gridBoard.sort(() => 0.5 - Math.random());
 let flippedCards = [];
 
-// Empty Card Array after clicks
-const emptyCards = function() {
-  flippedCards = [];
-}
+/**
+ * emptyCard - resets the flippedCards array
+ *
+ * @param {Array} flippedCards
+ */
+const emptyCards = () => { flippedCards = []; }
 
-// Card does not match
+/**
+ * cardNotMatched - if the cards do not match
+ */
 const cardNotMatched = function() {
   console.log( 'rude, no match' );
 
   emptyCards();
 }
 
-// Card is a match
+/**
+ * cardMatched - if the cards match
+ */
 const cardMatched = function() {
   console.log( 'it matches' );
 
@@ -37,7 +79,9 @@ const checkMatch = function( match ) {
   }
 }
 
-// Card is selected
+/**
+ * flipCard - if card is not already flipped, add class and call checkMatch
+ */
 const flipCard = function() {
   let match = this;
   
@@ -47,10 +91,23 @@ const flipCard = function() {
   }
 }
 
-// Add Event Listener to each card
-for( let eachCard of cards ) {
-  eachCard.addEventListener( 'click', flipCard );
+/**
+ * Generate cards
+ * 
+ * @param {Object} cards
+ */
+for( let eachCard of gridBoard ) {
+  const card = document.createElement('div');
+  const icon = document.createElement('span');
+
+  card.classList.add('tile');
+  icon.classList.add('icon');
+
+  card.dataset.type = eachCard.name;
+  icon.innerHTML = eachCard.id;
+
+  card.appendChild(icon);
+  game.appendChild(card);
+
+  card.addEventListener( 'click', flipCard );
 }
-
-
-
